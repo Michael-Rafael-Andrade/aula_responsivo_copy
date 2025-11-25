e10659d8808608706fd105b9f4ef1fbf590afa2e
Michael <michaelrafaeldeandrade@gmail.com>
2025-11-25 16:04:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function App() {
+
+  const { width, height } = useWindowDimensions();
+  const [accessibleMode, setAccessibleMode] = useState(false);
+  const isTablet = width > 700;
+  const fontSize = isTablet ? 24 : 16;
+  const padding = isTablet ? 30 : 15;
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider>  
       <ResponsiveApp />
     </SafeAreaProvider>
   );
@@ -13,7 +20,15 @@ export default function App() {
 
 function ResponsiveApp() {
   return <SafeAreaView style={styles.container} >
-
+    <Text style={styles.text}>
+      Este é um exemplo de app responsivo e acessível
+      {'\n'}(largura atual: {width.toFixed(0)});
+    </Text>
+    <View style={styles.buttonContainer}>
+      <Button
+        title={} // parei aqui 18:08 min do vídeo 2
+      />
+    </View>
   </SafeAreaView>
   const styles = StyleSheet.create({
     container: {
